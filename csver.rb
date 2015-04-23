adc495d2325ececb0ec835465158993b9c432831
Tom Meagher <hello+git@tommeagher.com>
2015-04-22 21:32:53 -0400

    handle pagination with tabula and removes docsplit. closes #15

--- csver.rb
+++ csver.rb
@@ -4,26 +4,19 @@
 require 'rubygems'
 require 'date'
 require 'fileutils'
-require 'docsplit'
 require 'tabula'
 require 'httparty'
-
-#require 'tmpdir'
 
 def scrape(file_name, csvs_output)
     puts "Processing "+file_name
     local_dir = "data/pdfs/"
     grab(file_name, local_dir)
     FileUtils.mkdir_p(csvs_output)
-
     peel(file_name, csvs_output)
 end
 
 def grab(file_name, local_dir)
-    pdfs_output = "data/burstpdfs/"
-
     if Dir.entries(local_dir).include?file_name
-        Docsplit.extract_pages(local_dir+"/"+file_name, :output=>pdfs_output)
     else
         retrieve(file_name, local_dir, remote_dir)
     end
@@ -37,20 +30,14 @@ def retreive(file_name, local_dir, remote_dir)
 end
 
 def peel(file_name, csvs_output)
-    pdfs_dir = "data/burstpdfs/"
+    pdfs_dir = 'data/pdfs/'
     counter = 0
-    Dir.foreach(pdfs_dir) do |item|
-        num_chars = file_name[0..-5].length
-	    page_area = [97.55,11.19,573.48,778.62]
-	    next if item.start_with?('.')
-	    next if item.end_with?('.txt')
-        next if item[0..num_chars-1]!=file_name[0..num_chars-1]
-        
-        extractor = Tabula::Extraction::ObjectExtractor.new(pdfs_dir+item, :all )
-        extractor.extract.each_with_index do |pdf_page, page_index|
-        
+    page_area = [97.55,11.19,573.48,778.62]
+    extractor = Tabula::Extraction::ObjectExtractor.new(pdfs_dir+file_name, :all )
+    extractor.extract.each_with_index do |pdf_page, page_index|
         first_csv = pdf_page.get_table.to_csv
         my_csv = CSV.parse(first_csv)
+        #TODO make this a function
         if my_csv[0][-1]!= ""
             date = my_csv[0][-1]
         else
@@ -66,6 +53,8 @@ def peel(file_name, csvs_output)
         release_date = Date.parse date
         covers = Date.parse coverage
 
+
+        #TODO make this a function
         year_pattern = /.*(201\d)/
         if file_name.end_with?('stats.pdf')
             outfile = csvs_output + release_date.to_s + "-for-" + id + "-" + year_pattern.match(file_name)[1] + "annual.csv"
@@ -74,12 +63,14 @@ def peel(file_name, csvs_output)
         end
 
         table_area = pdf_page.get_area(page_area).get_table.to_csv
+        #TODO Make this a function
         table_csv = CSV.parse(table_area)
 
         heds = ["crime", "month_last_yr", "month_this_yr", "ytd_last", "ytd_this_yr", "cleared", "juvie_cleared"]
         CSV.open(outfile,"wb") do |csv|
             csv << heds
             table_csv.each do |row|
+                #TODO make this a function
                 next if row[0].strip.end_with?("Total:")
                 next if row[0].strip.end_with?("Index")
                 next if row[0].strip.end_with?("Crime")
@@ -93,11 +84,10 @@ def peel(file_name, csvs_output)
                 end
             end
         end
-    extractor.close!
+        counter += 1
     end
-    counter += 1
-end
-puts counter.to_s + " pages from " + file_name + " converted to CSVs."
+    extractor.close!    
+    puts counter.to_s + " pages from " + file_name + " converted to CSVs."
 end
 
 csvs_output="data/csvs/"
